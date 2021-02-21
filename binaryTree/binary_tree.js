114d807b8b90e7b615a2a8ab096e728df8ccff73
inna <innafeldman01@gmail.com>
2021-02-21 21:52:46 +0200

    Binary tree - creating Node and Bst class

---
+++ binaryTree/binary_tree.js
@@ -0,0 +1,13 @@
+const binaryTree = ()=>{
+    function Node(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    function Bst(data) {
+        this.root = null;
+    }
+}
+
+module.exports = binaryTree;
